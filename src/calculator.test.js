b9cb89493eb2ea2aa22b655cd4d5416b67d916dc
Haddib <hb150794@gmail.com>
2018-10-10 15:36:03 +0000

    add() function can now handle 1 number

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -3,3 +3,7 @@ const add = require("./calculator");
 it("should return zero on an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+it("should return number when only that number is inserted into add()", () => {
+  expect(add(5)).toBe(5);
+});
