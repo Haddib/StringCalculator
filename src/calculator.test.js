4cec1eda5a6cab14c6a41773f311ad11d98efe55
Haddib <hb150794@gmail.com>
2018-10-10 15:51:13 +0000

    add() function can now handle 2 string numbers. fancy

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -5,5 +5,9 @@ it("should return zero on an empty string", () => {
 });
 
 it("should return number when only that number is inserted into add()", () => {
-  expect(add(5)).toBe(5);
+  expect(add("5")).toBe(5);
+});
+
+it("should return the sum of the 2 numbers entered into add()", () => {
+  expect(add("5,4")).toBe(9);
 });
